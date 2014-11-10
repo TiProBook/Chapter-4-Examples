3058ae3832a8469f5d2d9a054ebf6aeffedd3089
Ben Bahrenburg <ben.bahrenburg@gmail.com>
2014-11-09 21:46:07 -0800

    update event publisher

--- ManyNotes/app/lib/sync-event-publisher.js
+++ ManyNotes/app/lib/sync-event-publisher.js
@@ -1,22 +1,11 @@
 var Q = require("q");
 
-var agent = {
-	createEventRequest : function(event){
-		if(event !=undefined || event !=null){
-			return JSON.stringify(event);
-		}else{
-			return null;
-		}
-	}
-};
-
 var publisher = function(event){
-	event.modifyid = new Date().getTime(); // Force the event to published with the current time.	
-	var request = agent.createEventRequest(evt);  
-	if(request == null){
+	if(event !=undefined || event !=null){
 		return;
 	}
-	
+	event.modifyid = new Date().getTime(); // Force the event to published with the current time.	
+	var request = JSON.stringify(event);	
 	var defer = Q.defer();          	
 	Alloy.Globals.azure.InsertTable('noteEvents', request, function(data) {
 		deferred.resolve(data);				
